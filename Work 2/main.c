cf87e62e88e69d744db38efcd7ebd79120ccf308
PRODAM-GARAJ-450T <saryman1972@gmail.com>
2016-06-24 01:34:47 +0300

    FORMULA

--- Work 2/main.c
+++ Work 2/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 int main ()
 {
+        d = sqr(b) - 4*a*c;
+	if ( d > 0)
+    {
+     x1 = (-b - sqrt(d))/(2 * a);
+     x2 = (-b + sqrt(d))/(2 * a);
+     printf ("d > 0, x1 = %f, x2 = %f", x1, x2); 
+    }
+    else if (d = 0)
+    {
+     x = -b/2*a;
+     printf ("d = 0, X = %f ", x);  
+    }
+    else
+        printf ("d < 0, KORNEI NET");
 	return 0;
 }
