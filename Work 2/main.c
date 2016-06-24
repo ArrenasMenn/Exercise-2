6f2094d64ed85afbb6238c0f9df5964c6e487fd0
PRODAM-GARAJ-450T <saryman1972@gmail.com>
2016-06-24 14:12:59 +0300

    FINAL2

--- Work 2/main.c
+++ Work 2/main.c
@@ -2,25 +2,25 @@
 #include <math.h>
 int main ()
 {
-	double a, b, c, d, x1, x2, x;
+	int a, b, c, d, x1, x2, x;
 	printf ( "BBEDITE TRI CELbIX 4ISLA \n" );
-	scanf ( "%f", &a );
-	scanf ( "%f", &b );
-	scanf ( "%f", &c );
-        d = sqr(b) - 4*a*c;
+	scanf ( "%i", &a );
+	scanf ( "%i", &b );
+	scanf ( "%i", &c );
+    
+    d = pow(b,2) - 4*a*c;
 	if ( d > 0)
     {
      x1 = (-b - sqrt(d))/(2 * a);
      x2 = (-b + sqrt(d))/(2 * a);
-     printf ("d > 0, x1 = %f, x2 = %f", x1, x2); 
+     printf ("d > 0, x1 = %i, x2 = %i\n", x1, x2); 
     }
     else if (d = 0)
     {
      x = -b/2*a;
-     printf ("d = 0, X = %f ", x);  
+     printf ("d = 0, X = %i \n", x);  
     }
     else
-        printf ("d < 0, KORNEI NET");
+        printf ("d < 0, KORNEI NET\n");
 	return 0;
 }
-
